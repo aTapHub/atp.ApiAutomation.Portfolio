a629439773a44a028f363af5e513f9e9369cfb35
atapardea <atapardea@gmail.com>
2025-09-03 22:59:26 +0300

    first commit

---
+++ atp.ApiAutomation.Framework/atp.ApiAutomation.Framework/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace atp.ApiAutomation.Framework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
