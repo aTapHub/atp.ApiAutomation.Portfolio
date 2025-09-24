a78ebb1c2e210435b2bc0c0bf4bb5a015139f65d
atapardea <atapardea@gmail.com>
2025-09-25 00:05:06 +0300

    Add test report generation with ExtentReport

---
+++ atp.ApiAutomation.Framework/SetupFixture.cs
@@ -0,0 +1,46 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atp.ApiAutomation.Framework
+{
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        public static ExtentReports Extent { get; set; }
+
+        private static readonly string reportDirectory
+            = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestReport");
+
+        [OneTimeSetUp]
+        public void Setup() { 
+        
+            if (Directory.Exists(reportDirectory) )
+                {
+                Directory.Delete(reportDirectory, true);
+                }
+
+            Directory.CreateDirectory(reportDirectory);
+
+            Extent = new ExtentReports();
+
+            // Attach the HTML reporter (Spark is a popular choice)
+            var sparkReporter = new ExtentSparkReporter(Path.Combine(reportDirectory, "index.html"));
+            Extent.AttachReporter(sparkReporter);
+
+            Extent.AddSystemInfo(".NET Version", Environment.Version.ToString());
+            Extent.AddSystemInfo("OS", Environment.OSVersion.VersionString);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown() 
+        {
+            Extent.Flush();
+        }   
+
+    }
+}
--- atp.ApiAutomation.Framework/Tests/BaseTest.cs
+++ atp.ApiAutomation.Framework/Tests/BaseTest.cs
@@ -1,4 +1,5 @@
 ﻿using atp.ApiAutomation.Framework.Configurations;
+using AventStack.ExtentReports;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,6 +16,7 @@ namespace atp.ApiAutomation.Framework.Tests
         public static ApiSettings Settings { get; private set; }
         public RestClient client;
         public IServiceProvider ServiceProvider { get; set; }
+        public ExtentTest Test { get; set; }
 
 
         [OneTimeSetUp]
@@ -49,6 +51,40 @@ namespace atp.ApiAutomation.Framework.Tests
             ServiceProvider = services.BuildServiceProvider();
 
         }
+
+        [SetUp]
+        public void Setup()
+        {
+            Test = SetupFixture.Extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var message = TestContext.CurrentContext.Result.Message;
+            var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+
+            // Log the final status of the test.
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                // If the test failed, log the error message and stack trace.
+                Test.Fail($"Test Failed: {message}");
+                Test.Log(Status.Fail, stacktrace);
+            }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Skipped)
+            {
+                // If the test was skipped, log it.
+                Test.Skip($"Test Skipped: {message}");
+            }
+            else
+            {
+                // If the test passed, log it.
+                Test.Pass("Test Passed successfully.");
+            }
+        }
+
+
 
         [OneTimeTearDown]
         public void GlobalTearDown()
